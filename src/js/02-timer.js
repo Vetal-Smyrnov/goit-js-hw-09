15d35d477927fab8d8ab988bb9e66492d867bc42
Vitalii Smyrnov <113116677+Vetal-Smyrnov@users.noreply.github.com>
2023-03-15 19:15:50 +0200

    +

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,16 +4,16 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 const refs = {
   chooseDate: document.querySelector('input#datetime-picker'),
-  startCounterBtn: document.querySelector('button[data-start]'),
-  days: document.querySelector('span[data-days]'),
-  hours: document.querySelector('span[data-hours]'),
-  minutes: document.querySelector('span[data-minutes]'),
-  seconds: document.querySelector('span[data-seconds]'),
+  startCounterButton: document.querySelector('button[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
 };
 
 let selectDate;
-refs.startCounterBtn.disabled = true;
-refs.startCounterBtn.style.cursor = 'pointer';
+refs.startCounterButton.disabled = true;
+refs.startCounterButton.style.cursor = 'pointer';
 
 const options = {
   enableTime: true,
@@ -24,7 +24,7 @@ const options = {
     if (selectedDates <= Date.now()) {
       Notiflix.Notify.failure('Please choose a date in the future');
     } else {
-      refs.startCounterBtn.disabled = false;
+      refs.startCounterButton.disabled = false;
       selectDate = selectedDates;
     }
   },
@@ -60,19 +60,14 @@ class Timer {
   }
 
   convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
     const day = hour * 24;
+    const hour = minute * 60;
+    const minute = second * 60;
+    const second = 1000;
 
-    // Remaining days
     const days = Math.floor(ms / day);
-    // Remaining hours
     const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
     const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
     return { days, hours, minutes, seconds };
@@ -83,18 +78,18 @@ const timer = new Timer({
   onTick: updateCounter,
 });
 
-refs.startCounterBtn.addEventListener('click', timer.start.bind(timer));
+refs.startCounterButton.addEventListener('click', timer.start.bind(timer));
 
 flatpickr(refs.chooseDate, options);
 
-function updateCounter({ days, hours, minutes, seconds }) {
-  refs.days.textContent = `${addLeadingZero(days)}`;
-  refs.hours.textContent = `${addLeadingZero(hours)}`;
-  refs.minutes.textContent = `${addLeadingZero(minutes)}`;
-  refs.seconds.textContent = `${addLeadingZero(seconds)}`;
-  refs.startCounterBtn.setAttribute('disabled', true);
+function addLeadingZeros(value) {
+  return String(value).padStart(2, '0');
 }
 
-function addLeadingZero(value) {
-  return String(value).padStart(2, '0');
+function updateCounter({ days, hours, minutes, seconds }) {
+  refs.days.textContent = `${addLeadingZeros(days)}`;
+  refs.hours.textContent = `${addLeadingZeros(hours)}`;
+  refs.minutes.textContent = `${addLeadingZeros(minutes)}`;
+  refs.seconds.textContent = `${addLeadingZeros(seconds)}`;
+  refs.startCounterButton.setAttribute('disabled', true);
 }
