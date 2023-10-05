5990633b2843fd52ff59d6be2cd08030611b0394
Vetal-Smyrnov <113116677+Vetal-Smyrnov@users.noreply.github.com>
2023-10-05 15:14:55 +0000

    Deploying to gh-pages from @ Vetal-Smyrnov/goit-js-hw-09@aa9d1da670c5b1b4f575ce057f9fdc413332f91d 🚀

---
+++ 01-color-switcher.8ae4c1f5.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),d=document.body;let a=null;function l(){d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(()=>{l(),a=setInterval(l,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(()=>{clearInterval(a),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.8ae4c1f5.js.map
